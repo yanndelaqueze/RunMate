279afc127c7071d7ddb88a71102614fa2019a0f5
Yann Delaqueze <yann.delaqueze@gmail.com>
2023-03-06 16:28:21 +0100

    def dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,4 +3,12 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def dashboard
+    # Runs I created :
+    @runs = Run.where(user: current_user)
+    # Attendance requests I received :
+    @pending_requests = Attendance.joins(:run).where(run: { user_id: current_user }).where(attendance.pending)
+  end
+
 end
