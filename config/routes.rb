d650441092a83ff404e8f3047f78cfcd6de9e3a1
Yann Delaqueze <yann.delaqueze@gmail.com>
2023-03-07 12:02:23 +0100

    attendance controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,14 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: 'users/sessions' }
   root to: "pages#home"
 
-  resources :runs do
-    resources :attendances, only: %i[index create edit destroy]
-  end
-
   get '/dashboard', to: 'pages#dashboard'
+
+  resources :runs do
+    resources :attendances, only: %i[index new create update destroy] do
+      member do
+        patch 'confirm', to: "attendances#confirm"
+        patch 'decline', to: "attendances#decline"
+      end
+    end
+  end
 end
