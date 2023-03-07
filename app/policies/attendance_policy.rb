ab62cb8d57365e7b580e24017dc1421a3cfbb636
Milagros Lasarte <lasartemilagros@gmail.com>
2023-03-07 17:23:03 +0100

    add pundit attendances

--- app/policies/attendance_policy.rb
+++ app/policies/attendance_policy.rb
@@ -1,9 +1,8 @@
 class AttendancePolicy < ApplicationPolicy
   class Scope < Scope
-    # NOTE: Be explicit about which records you allow access to!
-    # def resolve
-    #   scope.all
-    # end
+    def resolve
+      scope.where(user: user)
+    end
   end
 
   def new?
