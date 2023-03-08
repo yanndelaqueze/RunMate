735ed3bfc29dc9a554a18dece9f4aefd9eae5703
Franck Stephane Ndzomga <ndzomgafs@gmail.com>
2023-03-08 16:23:04 +0100

    attendances back to normal

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_08_134032) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_08_152107) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,9 +18,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_08_134032) do
     t.integer "status"
     t.text "content"
     t.bigint "user_id", null: false
-    t.bigint "run_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "run_id", null: false
     t.index ["run_id"], name: "index_attendances_on_run_id"
     t.index ["user_id"], name: "index_attendances_on_user_id"
   end
