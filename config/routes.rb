969cdf569b7a286858bca08aaf7a44893ab09516
Franck Stephane Ndzomga <ndzomgafs@gmail.com>
2023-03-08 12:11:42 +0100

    user profile and show

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,11 @@ Rails.application.routes.draw do
   root to: "pages#home"
 
   get '/dashboard', to: 'pages#dashboard'
+
   devise_scope :user do
     get '/users/profile', to: 'users/sessions#show', as: 'user_profile'
+    get '/users/:id/edit', to: 'users/sessions#edit', as: 'edit_user'
+    patch '/users/:id/edit', to: 'users/sessions#update', as: 'update_user'
   end
 
   resources :runs do
