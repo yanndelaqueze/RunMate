ee406f58da4bff1b59fc3a073c2cbb8e91c27de5
Alizee Vallat <alizee.v@live.fr>
2023-03-08 14:14:10 +0100

    ongoing reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,13 +1,28 @@
 class ReviewsController < ApplicationController
 
   def new
-    raise
+    @attendance = Attendance.find(params[:attendance_id])
     @review = Review.new
+    @review.attendance = @attendance
     authorize @review
   end
 
   def create
-    @run = Run.new
-    @review = Review.new
+    @attendance = Attendance.find(params[:attendance_id])
+    @user = current_user
+    @review = Review.new(review_params)
+    @review.attendance = @attendance
+    @review.user = @user
+    if @review.save
+      redirect_to
+    else
+      redirect_to (@review), status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
   end
 end
