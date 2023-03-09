9fd4b6df4c67a928182ddbc54d258e09b65c2c93
Franck Stephane Ndzomga <ndzomgafs@gmail.com>
2023-03-09 11:39:37 +0100

    stimulus controller for address autocomplete

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,6 +4,9 @@
 
 import { application } from "./application"
 
+import AddressAutocompleteController from "./address_autocomplete_controller"
+application.register("address-autocomplete", AddressAutocompleteController)
+
 import FormSubmitController from "./form_submit_controller"
 application.register("form-submit", FormSubmitController)
 
