8cdbf7f2e93347f2988347bb4cfdbd22102f4c30
Franck Stephane Ndzomga <ndzomgafs@gmail.com>
2023-03-13 12:35:06 +0100

    future runs only

--- app/controllers/runs_controller.rb
+++ app/controllers/runs_controller.rb
@@ -24,21 +24,31 @@ class RunsController < ApplicationController
   end
 
   def search
+    @runs = Run.where('date > ?', DateTime.now)
+
     Time.zone = "Europe/Paris"
+
     t = Time.zone.parse(params[:hour]) || Time.zone.parse("12:00")
+
     params[:start_date].present? ? d1 = Time.zone.parse(params[:start_date]).to_date : d1 = Time.now.to_date
     params[:end_date].present? ? d2 = Time.zone.parse(params[:end_date]).to_date : d2 = d1 + 5
+
     d1 = Time.zone.parse("#{d1.strftime('%F')} #{t.strftime('%T')}")
     d2 = Time.zone.parse("#{d2.strftime('%F')} #{t.strftime('%T')}")
+
     time_added = Time.parse("02:00:00").seconds_since_midnight.seconds
     d2 += time_added
+
     if params[:query].present?
       @runs = Run.near(params[:query], 10, units: :km, order: :distance)
                        .reverse_order
+
       @runs = @runs.select { |run| (d1..d2).cover?(run.date) } if params[:date_start].present?
     else
       if params[:start_date].present? || params[:hour].present?
-        @runs = Run.all.select { |run| (d1..d2).cover?(run.date) }
+
+        @runs = @runs.select { |run| (d1..d2).cover?(run.date) }
+
       else
         @runs = []
       end
