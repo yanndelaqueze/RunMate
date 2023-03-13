c22911ee733936c1eb6b43fde8f7c2a5e2ef21e9
Alizee Vallat <alizee.v@live.fr>
2023-03-13 16:44:38 +0100

    reviews - profile

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,6 @@ class Review < ApplicationRecord
   belongs_to :attendance
   has_one :user, through: :attendance
   has_one :run, through: :attendance
+  validates :rating, presence: true
+  validates :comment, presence: true, length: { minimum: 3 }
 end
