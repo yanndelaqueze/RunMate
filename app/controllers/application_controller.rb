4e8a11f8e30113a5dd03aa0c0ac4f36fdcf2d640
Alizee Vallat <alizee.v@live.fr>
2023-03-15 12:31:07 +0100

    fix path from sign in to index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,10 @@ class ApplicationController < ActionController::Base
   #   redirect_to(root_path)
   # end
 
+  def after_sign_in_path_for(resource)
+    runs_path # your path
+  end
+
   private
 
   def skip_pundit?
