ca93bb10a8f5d7ec1a61a8f1a303debb100146ee
Yann Delaqueze <yann.delaqueze@gmail.com>
2023-07-04 10:45:23 +0200

    a bit of CSS

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -22,7 +22,7 @@ export default class extends Controller {
   }
   #addMarkersToMap() {
     this.markersValue.forEach((marker) => {
-      const popup = new mapboxgl.Popup({offset: 15}).setHTML(marker.run_info_map_html)
+      const popup = new mapboxgl.Popup({offset: 15}).setHTML(marker.city_info_map_html)
       const customMarker = document.createElement("div")
       customMarker.innerHTML = marker.run_marker_html
       new mapboxgl.Marker(customMarker)
